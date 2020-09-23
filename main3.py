78fcd22c2bc320a32d4ebe2eced658f7f93d72e8
gyurza921 <objekt.172.uru@gmail.com>
2020-09-23 21:29:42 +0900

    重複した要素を統合する処理を追加

--- main3.py
+++ main3.py
@@ -1,15 +1,14 @@
 # coding : utf-8
 # 文字コードの宣言
-#買い物プログラム
-# クラスの応用
-# 関数の応用
+# 買い物プログラム
 # 買い物かごの内容を管理
+# 重複した要素の検知
 
-# 残金
-money = 0
+# 買い物かごクラス
+class Cart():
+    def __init__(self):
+        self.contents = []
 
-# 購入した商品のリスト
-cart = []
 
 # 買い物かごの内容のクラス
 class Cart_content():
@@ -17,11 +16,17 @@ class Cart_content():
         self.product = product
         self.number = number
 
-# 商品毎の単価
+# 商品のクラス
 class Product():
     def __init__(self, name, price):
         self.name = name 
         self.price = price
+
+# 残金
+money = 0
+
+# 購入した商品のリスト
+cart = []
 
 # りんご
 apple = Product("りんご", 500)
@@ -33,7 +38,7 @@ banana = Product("バナナ", 400)
 grape = Product( "ぶどう",300)
 
 # 果物のリスト
-fruits = [apple, banana, grape]
+fruits = (apple, banana, grape)
 
 # 個数
 count = 0
@@ -81,7 +86,26 @@ def select_product():
             print(val.product.name + "単価 ￥" + str(val.product.price) + "\n購入数" + str(val.number))
 
     # 買い物かごから中身を出す
-    
+    decision = input("買い物かごの中の商品の購入を中断しますか？　（はい：０、いいえ：１）")
+    decision = int(decision)
+    if decision == 0:
+
+        num = 0
+        for val in cart:
+            print(str(num) + val.product.name + "単価 ￥" + str(val.product.price) + "\n購入数" + str(val.number))
+            num += 1
+        num = 0
+
+        selection = input('''購入を取りやめる商品を選んでください''')
+        selection = int(selection)
+        del cart[selection]
+
+        num = 0
+        for val in cart:
+            print(str(num) + val.product.name + "単価 ￥" + str(val.product.price) + "\n購入数" + str(val.number))
+            num += 1
+        num = 0   
+
     # 購入を続けるか終わるかを選択する
     decision = input("商品の購入を続けますか？　（はい：０、いいえ：１）")
 
@@ -91,6 +115,21 @@ def select_product():
 
 # 動作確認
 select_product()
+
+# 重複する要素の検知
+for val1 in cart:
+    print(val1)
+    print(val1.product.name)
+    i = 0
+    for val2 in cart:
+        print(val2)
+        print(val2.product.name)
+        if val1.product.name == val2.product.name and val1 != val2:
+            print("この要素は重複しています")
+            val1.number = val1.number + val2.number
+            del cart[i]
+            print("重複した要素を統合しました")
+        i += 1
 
 for val in cart:
     print(val.product.name + "単価 ￥" + str(val.product.price) + "\n購入数" + str(val.number))
